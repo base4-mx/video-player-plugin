5754e63edeefeec1f6e1c37a832abd206162d449
base4 <hola@base4.mx>
2024-08-02 14:01:30 -0600

    Add files via upload

---
+++ video-player-plugin/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// ENG - Requires the file that contains the definition of the VideoJsViewerPlugin class
+// ESP - Requiere el archivo que contiene la definición de la clase VideoJsViewerPlugin
+
+require_once('VideoJsViewerPlugin.inc.php');
+
+// ENG - Creates a new instance of the VideoJsViewerPlugin and returns it
+// ESP - Crea una nueva instancia de VideoJsViewerPlugin y la retorna
+
+return new VideoJsViewerPlugin();
+
+?>
